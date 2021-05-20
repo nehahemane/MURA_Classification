b3f7958b90ce341ed43a6fb2bb96a4fab3a6d991
Neha Hemane <76604517+nehahemane@users.noreply.github.com>
2021-05-20 13:26:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -51,5 +51,6 @@ def predict():
 #    port = int(os.environ.get('PORT', 5000))
 #    app.run(debug = True, host='0.0.0.0', port=port)
 if __name__ == '__main__':
-    app.run(debug = True, host='127.0.0.1', port=8000)
+    port = int(os.environ.get('PORT', 8000))
+    app.run(debug = True, host='127.0.0.1', port=port)
     
